6bad4128dbbac08f5d6753ad48938a15c89d2b60
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-13 23:20:39 +0100

    seeded db post with faker data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         $this-> call([
             PersonSeeder :: class,
             PersonDetailSeeder :: class,
+            PostSeeder :: class
             
         ]);
     }
