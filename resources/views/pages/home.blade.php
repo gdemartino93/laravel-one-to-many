531ace57a0c1ec101238dbcb834f672d163508a1
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-14 01:21:37 +0100

    create home with route to page post and ppl

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,6 +10,10 @@
         Saepe officia quidem velit quod optio? Assumenda veniam, quis ut itaque laudantium consequuntur. Error doloribus earum, veritatis sed cupiditate nulla id repudiandae ipsa officiis magni laudantium, inventore ex architecto cumque.
         Dolorem dignissimos quia aliquid odio nemo obcaecati quam explicabo, aliquam velit minus ut corrupti tenetur dicta quae labore similique iste molestiae, inventore ipsam deleniti ea ab dolore voluptatum tempore. Aliquid?
         Numquam, consequatur. Error, molestiae? Vitae nesciunt pariatur est iste praesentium reprehenderit atque eos. Inventore soluta architecto quibusdam sequi a eveniet, dignissimos nisi officia praesentium consectetur tenetur harum rerum aliquam ea!</p>
+        <div class="d-flex justify-content-around">
+            <a href="{{route('pages.person')}}">Person</a>
+            <a href="{{route('pages.posts')}}">Post</a>
+        </div>
 </div>
 
 @endsection
